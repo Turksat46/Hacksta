18f0c9ac7a4ce82ba70b4d4c70f403b5bcadf029
Fadester <53977560+Fadester@users.noreply.github.com>
2020-11-18 18:36:04 +0100

    Update profilepic.sh

--- Linux/tools/profilepic.sh
+++ Linux/tools/profilepic.sh
@@ -10,7 +10,7 @@ echo ""
 read -p $'\033[1;36m[\033[1;32m+\033[1;36m]\033[0;34m Username: \e[0m' user
 echo -e "$lightcyan[$green+$lightcyan]$Blue Downloading... $farblos"
 curl -b cookie.$user -L -O https://instagram.com/$user > /dev/null 2>&1
-sed -n ''184','184'p' $dir/$user | cut -d '"' -f4 > profilepicture.$user
+sed -n ''185','185'p' $dir/$user | cut -d '"' -f4 > profilepicture.$user
 prof_pic=$(cat profilepicture.$user)
 curl "$prof_pic" --output $dir/output/$user > /dev/null 2>&1
 rm profilepicture.$user
